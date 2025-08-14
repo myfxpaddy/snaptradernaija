78e7886686ce081c1fcfeae2e87c68e216f54cf6
myfxpaddy <obasijoseph114@gmail.com>
2025-08-14 19:50:07 +0100

    Fix Firebase storage bucket to appspot.com

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -4,8 +4,8 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyCjNiHMO20pntRKG7tInqJnVh5h0NEDS5Y",
   authDomain: "snaptradernaija-e5387.firebaseapp.com",
   projectId: "snaptradernaija-e5387",
-  storageBucket: "snaptradernaija-e5387.firebasestorage.app",   
-  messagingSenderId: "218782972162",                 
-  appId: "1:218782972162:web:ad55c70132c2eb778aaa1b",                                
-  measurementId: "G-47EQLFGM9T"                 
+  storageBucket: "snaptradernaija-e5387.appspot.com",  // FIXED
+  messagingSenderId: "218782972162",
+  appId: "1:218782972162:web:ad55c70132c2eb778aaa1b",
+  measurementId: "G-47EQLFGM9T"
 };
