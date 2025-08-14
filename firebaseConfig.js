ada2e81060e0d365ca3f9b2f4bcd5d4e6f106e25
myfxpaddy <obasijoseph114@gmail.com>
2025-08-14 18:09:59 +0100

    Add Firebase web config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,12 @@
-/* Replace with your Firebase Web App config (from Firebase console) */
-window.FIREBASE_CONFIG = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+// ~/Projects/snaptradernaija/firebaseConfig.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiNiHM002pntRKG7tInqJnVh5h9NED5Y",
+  authDomain: "snaptradernaija-e5387.firebaseapp.com",
+  projectId: "snaptradernaija-e5387",
+  storageBucket: "snaptradernaija-e5387.appspot.com",
+  messagingSenderId: "217882972162",
+  appId: "1:217882972162:web:ad55c7e0132c2eb778aaa1",
+  measurementId: "G-47EQLFGM9T"
 };
+
